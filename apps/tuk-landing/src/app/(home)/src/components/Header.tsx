01825ac9af03b2882ac4dc793c88db4c819b4158
sikkzz <sling0623@gmail.com>
2025-08-07 01:54:02 +0900

    feat: 서비스 랜딩 페이지 앱 소개 타이틀 섹션 퍼블리싱

--- apps/tuk-landing/src/app/(home)/src/components/Header.tsx
+++ apps/tuk-landing/src/app/(home)/src/components/Header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="fixed inset-0 z-[999] px-1.5">
-      <div className="m-auto flex h-[3.125rem] max-w-7xl items-center justify-between overflow-hidden px-[4.375rem] max-[1280px]:max-w-[55rem] max-[1280px]:px-6">
+    <header className="fixed inset-0 z-[999] h-[3.125rem] px-1.5">
+      <div className="m-auto flex max-w-7xl items-center justify-between overflow-hidden px-[4.375rem] max-[1280px]:max-w-[55rem] max-[1280px]:px-6">
         <Link href="/" className="serif-title-24-M flex px-2 py-2.5 text-white-default">
           TUK
         </Link>
