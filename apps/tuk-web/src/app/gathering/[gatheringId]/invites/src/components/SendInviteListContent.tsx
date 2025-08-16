4f0b836bf3b1063b88e25c7a648329acec9fdd46
sikkzz <sling0623@gmail.com>
2025-08-17 03:17:42 +0900

    feat: 보낸 초대장 리스트 상세 페이지 추가

--- apps/tuk-web/src/app/gathering/[gatheringId]/invites/src/components/SendInviteListContent.tsx
+++ apps/tuk-web/src/app/gathering/[gatheringId]/invites/src/components/SendInviteListContent.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 import { useMemo } from 'react';
 
 import { gatheringProposalAPIService } from '@/app/gathering/[gatheringId]/invites/src/service';
@@ -41,7 +42,9 @@ const SendInviteListContent = () => {
         <>
           {proposals.map(proposal => (
             <div className="flex flex-col items-center gap-2.5" key={proposal.proposalId}>
-              <Card proposalData={proposal} />
+              <Link href={`/proposal/${proposal.proposalId}/detail`}>
+                <Card proposalData={proposal} />
+              </Link>
 
               <span className="pretendard-body-12-R text-gray-800">{proposal.relativeTime}</span>
             </div>
