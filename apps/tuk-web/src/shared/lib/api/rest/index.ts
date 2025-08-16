3c7b6a79fe45beb142e46ec618e559ecf9f8b722
sikkzz <sling0623@gmail.com>
2025-08-17 01:27:09 +0900

    feat: 초대장 상세 페이지 ui, url 변경

--- apps/tuk-web/src/shared/lib/api/rest/index.ts
+++ apps/tuk-web/src/shared/lib/api/rest/index.ts
@@ -6,15 +6,26 @@ import { getBridgeSender } from '@/shared/lib/app-bridge/bridgeSender';
 export { RestAPI } from './rest';
 export type { RestAPIConfig, RestAPIProtocol } from './types';
 
+// const defaultJsonInstance = (baseURL: string) =>
+//   new RestAPIInstance(baseURL, {
+//     withCredentials: false,
+//     authHeader: () => {
+//       if (typeof window === 'undefined') return null;
+//       const token = sessionStorage.getItem('accessToken');
+//       return token ? { Authorization: `Bearer ${token}` } : null;
+//     },
+//     bridgeSend: msg => getBridgeSender()?.(msg),
+//   });
+
 const defaultJsonInstance = (baseURL: string) =>
   new RestAPIInstance(baseURL, {
     withCredentials: false,
     authHeader: () => {
       if (typeof window === 'undefined') return null;
-      const token = sessionStorage.getItem('accessToken');
-      return token ? { Authorization: `Bearer ${token}` } : null;
+      const token =
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMiIsIm1lbWJlcklkIjoiMzIiLCJ0b2tlblR5cGUiOiJBQ0NFU1MiLCJpYXQiOjE3NTUwMDM4NTQsImV4cCI6MTc1NzU5NTg1NH0.sXHpNsim8-nIUdCjnAcOIZGsg2gUo8rnpkBmN-z5dKU';
+      return token ? { Authorization: `Bearer ${token.trim()}` } : null;
     },
-    bridgeSend: msg => getBridgeSender()?.(msg),
   });
 
 const formInstance = (baseURL: string) =>
