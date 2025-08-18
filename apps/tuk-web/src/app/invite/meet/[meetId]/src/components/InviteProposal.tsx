d48fb83e588b43775a8afda0b523aca41a825657
sikkzz <sling0623@gmail.com>
2025-08-18 21:15:03 +0900

    fix: background template 컴포넌트 추가

--- apps/tuk-web/src/app/invite/meet/[meetId]/src/components/InviteProposal.tsx
+++ apps/tuk-web/src/app/invite/meet/[meetId]/src/components/InviteProposal.tsx
@@ -4,46 +4,43 @@ import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
-import GradientBackground from '@/app/invite/meet/[meetId]/src/components/GradientBackground';
 import InviteProposalContent from '@/app/invite/meet/[meetId]/src/components/InviteProposalContent';
 import InviteProposalErrorFallback from '@/app/invite/meet/[meetId]/src/components/InviteProposalErrorFallback';
 import InviteProposalSkeleton from '@/app/invite/meet/[meetId]/src/components/InviteProposalSkeleton';
 import SkeletonGuard from '@/app/invite/meet/[meetId]/src/components/SkeletonGuard';
-import { Button } from '@/shared/components';
+import { BackgroundTemplate, Button } from '@/shared/components';
 import { useParam } from '@/shared/hooks/useParam';
 
 const InviteProposal = () => {
   const proposalId = Number(useParam('meetId'));
 
   return (
-    <div className="relative w-full overflow-y-auto overflow-x-hidden bg-gradient-to-b from-white-default to-[#DCC8F8] px-5">
-      <GradientBackground />
+    <BackgroundTemplate>
+      <BackgroundTemplate.Main className="overflow-y-auto px-5">
+        <BackgroundTemplate.Gradient />
 
-      <QueryErrorResetBoundary>
-        {({ reset }) => (
-          <ErrorBoundary onReset={reset} FallbackComponent={InviteProposalErrorFallback}>
-            <SkeletonGuard minMs={250} skeleton={<InviteProposalSkeleton />}>
-              <Suspense fallback={<InviteProposalSkeleton />}>
-                <InviteProposalContent />
-              </Suspense>
-            </SkeletonGuard>
-          </ErrorBoundary>
-        )}
-      </QueryErrorResetBoundary>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary onReset={reset} FallbackComponent={InviteProposalErrorFallback}>
+              <SkeletonGuard minMs={250} skeleton={<InviteProposalSkeleton />}>
+                <Suspense fallback={<InviteProposalSkeleton />}>
+                  <InviteProposalContent />
+                </Suspense>
+              </SkeletonGuard>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
 
-      <div className="mt-[4.125rem] flex justify-center pb-[9.0625rem]">
-        <TukLogo />
-      </div>
-
-      <div className="fixed bottom-0 left-1/2 z-30 w-full max-w-[600px] -translate-x-1/2 bg-gradient-to-b from-white-default/0 to-white-default px-5 py-6">
-        <Button
-          className="w-full"
-          onClick={() => (window.location.href = `tuk-app://tuk/proposal-detail/${proposalId}`)}
-        >
-          초대장 확인하기
-        </Button>
-      </div>
-    </div>
+        <BackgroundTemplate.CTA>
+          <Button
+            className="w-full"
+            onClick={() => (window.location.href = `tuk-app://tuk/proposal-detail/${proposalId}`)}
+          >
+            초대장 확인하기
+          </Button>
+        </BackgroundTemplate.CTA>
+      </BackgroundTemplate.Main>
+    </BackgroundTemplate>
   );
 };
 
@@ -113,42 +110,6 @@ export const InviteCard = () => (
         <stop stopColor="white" stopOpacity="0.4" />
         <stop offset="1" stopColor="#999999" stopOpacity="0.2" />
       </linearGradient>
-    </defs>
-  </svg>
-);
-
-export const TukLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="154" height="76" viewBox="0 0 154 76" fill="none">
-    <foreignObject x="-4" y="-4" width="162" height="84">
-      <div
-        style={{
-          backdropFilter: 'blur(2px)',
-          clipPath: 'url(#bgblur_0_1031_14686_clip_path)',
-          height: '100%',
-          width: '100%',
-        }}
-      />
-    </foreignObject>
-    <g opacity="0.2" data-figma-bg-blur-radius="4">
-      <path
-        d="M100.67 52.9516L145.066 0H145.337L100.94 52.9516H100.67ZM84.3367 74.5098V74.3111H108.701V74.5098H84.3367ZM114.747 74.5098V74.3111H142.72V74.5098H114.747ZM92.0069 74.5098L107.347 0H115.92L100.579 74.5098H92.0069ZM125.395 74.5098L114.296 36.7582L120.793 29.2078L134.779 74.5098H125.395ZM99.2259 0.198696V0H123.59V0.198696H99.2259ZM132.343 0.198696V0H154V0.198696H132.343Z"
-        fill="#1F1F1F"
-      />
-      <path
-        d="M69.7309 76C64.828 76 60.7523 75.1059 57.5038 73.3176C54.2853 71.5294 52.0745 68.8305 50.8713 65.2209C49.6682 61.6113 49.6381 57.0911 50.7811 51.6601L61.6096 0H70.1821L59.3536 50.6667C58.6919 53.7795 58.3159 56.8261 58.2257 59.8065C58.1655 62.7538 58.4964 65.4362 59.2183 67.8536C59.9703 70.2379 61.2336 72.142 63.0083 73.566C64.7829 74.99 67.1742 75.702 70.1821 75.702C74.8143 75.702 78.6193 74.7582 81.5972 72.8706C84.6051 70.9499 87.0114 68.2013 88.8162 64.6248C90.6209 61.0484 92.0497 56.7268 93.1024 51.6601L103.931 0H104.111L93.2829 51.6601C92.2001 56.8593 90.7262 61.2802 88.8613 64.9229C87.0265 68.5325 84.56 71.281 81.4618 73.1686C78.3937 75.0562 74.4835 76 69.7309 76ZM53.037 0.198696V0H78.7547V0.198696H53.037ZM94.5462 0.198696V0H112.143V0.198696H94.5462Z"
-        fill="#1F1F1F"
-      />
-      <path
-        d="M11.5504 74.5098L27.2517 0H35.8242L20.1229 74.5098H11.5504ZM1.62427 74.5098V74.3111H30.0491V74.5098H1.62427ZM0 22.3529L4.69234 0H58.3836L53.6912 22.3529H53.5108C54.3831 18.1473 54.7591 14.3721 54.6387 11.0274C54.5184 7.68279 53.6612 5.05011 52.067 3.12942C50.4728 1.1756 47.9161 0.198696 44.3968 0.198696H17.7768C15.1298 0.198696 12.8438 0.76166 10.9187 1.88759C8.99366 3.01351 7.35435 4.58649 6.00079 6.60654C4.64723 8.59347 3.50422 10.9281 2.57177 13.6105C1.63931 16.2928 0.842216 19.207 0.180475 22.3529H0Z"
-        fill="#1F1F1F"
-      />
-    </g>
-    <defs>
-      <clipPath id="bgblur_0_1031_14686_clip_path" transform="translate(4 4)">
-        <path d="M100.67 52.9516L145.066 0H145.337L100.94 52.9516H100.67ZM84.3367 74.5098V74.3111H108.701V74.5098H84.3367ZM114.747 74.5098V74.3111H142.72V74.5098H114.747ZM92.0069 74.5098L107.347 0H115.92L100.579 74.5098H92.0069ZM125.395 74.5098L114.296 36.7582L120.793 29.2078L134.779 74.5098H125.395ZM99.2259 0.198696V0H123.59V0.198696H99.2259ZM132.343 0.198696V0H154V0.198696H132.343Z" />
-        <path d="M69.7309 76C64.828 76 60.7523 75.1059 57.5038 73.3176C54.2853 71.5294 52.0745 68.8305 50.8713 65.2209C49.6682 61.6113 49.6381 57.0911 50.7811 51.6601L61.6096 0H70.1821L59.3536 50.6667C58.6919 53.7795 58.3159 56.8261 58.2257 59.8065C58.1655 62.7538 58.4964 65.4362 59.2183 67.8536C59.9703 70.2379 61.2336 72.142 63.0083 73.566C64.7829 74.99 67.1742 75.702 70.1821 75.702C74.8143 75.702 78.6193 74.7582 81.5972 72.8706C84.6051 70.9499 87.0114 68.2013 88.8162 64.6248C90.6209 61.0484 92.0497 56.7268 93.1024 51.6601L103.931 0H104.111L93.2829 51.6601C92.2001 56.8593 90.7262 61.2802 88.8613 64.9229C87.0265 68.5325 84.56 71.281 81.4618 73.1686C78.3937 75.0562 74.4835 76 69.7309 76ZM53.037 0.198696V0H78.7547V0.198696H53.037ZM94.5462 0.198696V0H112.143V0.198696H94.5462Z" />
-        <path d="M11.5504 74.5098L27.2517 0H35.8242L20.1229 74.5098H11.5504ZM1.62427 74.5098V74.3111H30.0491V74.5098H1.62427ZM0 22.3529L4.69234 0H58.3836L53.6912 22.3529H53.5108C54.3831 18.1473 54.7591 14.3721 54.6387 11.0274C54.5184 7.68279 53.6612 5.05011 52.067 3.12942C50.4728 1.1756 47.9161 0.198696 44.3968 0.198696H17.7768C15.1298 0.198696 12.8438 0.76166 10.9187 1.88759C8.99366 3.01351 7.35435 4.58649 6.00079 6.60654C4.64723 8.59347 3.50422 10.9281 2.57177 13.6105C1.63931 16.2928 0.842216 19.207 0.180475 22.3529H0Z" />
-      </clipPath>
     </defs>
   </svg>
 );
